8424bef644af4fb7183d50aac1a9f28a06cebdc1
stephanos <github@stephanbehnke.de>
2014-04-26 13:54:09 +0200

    added HomeDir function

--- file.go
+++ file.go
@@ -1,9 +1,25 @@
 package file
 
-import "os"
+import (
+	"os"
+	"runtime"
+)
 
 // Exists returns whether the given file or directory exists or not.
 func Exists(path string) bool {
 	_, err := os.Stat(path)
 	return !os.IsNotExist(err)
+}
+
+// Return the path to the current user's home directory.
+func HomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+
+	return os.Getenv("HOME")
 }
